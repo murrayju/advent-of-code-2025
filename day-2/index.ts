4276615519eef5cbd3071a50f4dedc8a486ecd3f
Justin Murray <justin@tigerdata.com>
2025-12-04 15:07:09 -0500

    day 2 part 1 (optimized)

--- day-2/index.ts
+++ day-2/index.ts
@@ -10,22 +10,47 @@ const ranges = data
   .split(',')
   .map((range) => range.split('-').map((num) => parseInt(num, 10)));
 
+console.log(`Number of ranges: ${ranges.length}`); // 31
+
 const invalidIds = [];
+let iterations = 0;
 for (const [start, end] of ranges) {
   if (!start || !end || start > end) {
     throw new Error(`Invalid range: ${start}-${end}`);
   }
-  for (let i = start; i <= end; i++) {
+
+  let i = start;
+  while (i <= end) {
+    iterations += 1;
     const numStr = i.toString();
-    if (
-      numStr.length % 2 === 0 &&
-      numStr.slice(0, numStr.length / 2) === numStr.slice(numStr.length / 2)
-    ) {
+    if (numStr.length % 2) {
+      // odd length can't be doubled pattern
+      // jump to next power of 10 (even number of digits)
+      const left = 10 ** Math.floor(numStr.length / 2);
+      i = parseInt(`${left}${left}`, 10);
+      continue;
+    }
+    const left = numStr.slice(0, numStr.length / 2);
+    const right = numStr.slice(numStr.length / 2);
+    const leftNum = parseInt(left, 10);
+    const rightNum = parseInt(right, 10);
+    if (rightNum < leftNum) {
+      // jump to next doubled pattern
+      i = parseInt(`${left}${left}`, 10);
+      continue;
+    }
+
+    if (left === right) {
       invalidIds.push(i);
     }
+
+    const nextLeft = leftNum + 1;
+    // Consider the next doubled pattern
+    i = parseInt(`${nextLeft}${nextLeft}`, 10);
   }
 }
 
+console.log(`iterations: ${iterations}`); // 1949832 -> 799
 console.log(`Number of invalid IDs: ${invalidIds.length}`); // 768
 const sum = invalidIds.reduce((acc, curr) => acc + curr, 0);
 console.log(`Sum of invalid IDs: ${sum}`); // 15873079081
