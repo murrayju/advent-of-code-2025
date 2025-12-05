225452730faf08aef813de3c005d8efa90ea777c
Justin Murray <justin@tigerdata.com>
2025-12-05 12:53:35 -0500

    day 3 part 1 (optimized)

--- day-3/README.md
+++ day-3/README.md
@@ -1,3 +1,5 @@
 # Day 3 Notes
 
 The puzzle input is 200 lines of 100 digits. Assuming each digit might need to be inspected in the worst case, the upper bound for an optimal solution is 20,000 operations. My brute force solution produced 24k operations, but I do think the input is far from the worst case.
+
+After optimizing, got 17238 iterations, which is a good improvement. Because each iteration looks at both digits, it is actually 200 * 99 = 19800 loops in the worst case. Getting less than that means a significant number of the lines had a value of 99 early on.
--- day-3/index.ts
+++ day-3/index.ts
@@ -13,36 +13,44 @@ const banks = data
 console.log(`Number of banks: ${banks.length}`); // 200
 
 let iterations = 0;
-const findMaxDigitIndex = (
-  digits: number[],
-  startIndex: number,
-  endIndex: number,
-): [number, number] => {
-  if (startIndex < 0 || endIndex >= digits.length || startIndex > endIndex) {
-    throw new Error(`Invalid indices: ${startIndex}, ${endIndex}`);
-  }
-  let maxIndex = startIndex;
-  for (let i = startIndex + 1; i <= endIndex; i++) {
-    iterations++;
-    if (digits[i]! > digits[maxIndex]!) {
-      maxIndex = i;
-    }
-    if (digits[maxIndex] === 9) break;
-  }
-  return [maxIndex, digits[maxIndex]!];
-};
 
 const maxJoltages = banks.map((bank): number => {
-  const [firstIndex, firstDigit] = findMaxDigitIndex(bank, 0, bank.length - 2);
-  const [, secondDigit] = findMaxDigitIndex(
-    bank,
-    firstIndex + 1,
-    bank.length - 1,
-  );
-  return firstDigit * 10 + secondDigit;
+  if (bank.length < 2) {
+    throw new Error(`Bank has fewer than 2 digits: ${bank.join('')}`);
+  }
+  let v1: number = bank[0] || 0;
+  let v2: number = bank[1] || 0;
+
+  let i = 1;
+  while (i < bank.length - 1) {
+    iterations++;
+    const current = bank[i];
+    if (!current) throw new Error(`Invalid digit at index ${i}`);
+    const next = bank[i + 1];
+    if (!next) throw new Error(`Invalid digit at index ${i + 1}`);
+
+    if (current > v1) {
+      v1 = current;
+      v2 = next;
+      i++;
+      continue;
+    }
+
+    if (next > v2) {
+      v2 = next;
+    }
+
+    if (v1 === 9 && v2 === 9) {
+      break;
+    }
+
+    i++;
+  }
+
+  return v1 * 10 + v2;
 });
 
 console.log('\n---- Part 1 ----');
-console.log(`iterations: ${iterations}`); // 23976
+console.log(`iterations: ${iterations}`); // 23976 -> 17238
 const part1Sum = maxJoltages.reduce((acc, curr) => acc + curr, 0);
 console.log(`Sum of max joltages (answer): ${part1Sum}`); // 17155
