1685c88fe963b69c18b01817163d2ccb805cf553
Justin Murray <justin@tigerdata.com>
2025-12-04 22:26:45 -0500

    day 2 part 2 (brute)

--- day-2/index.ts
+++ day-2/index.ts
@@ -12,45 +12,83 @@ const ranges = data
 
 console.log(`Number of ranges: ${ranges.length}`); // 31
 
-const invalidIds = [];
-let iterations = 0;
-for (const [start, end] of ranges) {
-  if (!start || !end || start > end) {
-    throw new Error(`Invalid range: ${start}-${end}`);
+const part1 = () => {
+  const invalidIds = [];
+  let iterations = 0;
+  for (const [start, end] of ranges) {
+    if (!start || !end || start > end) {
+      throw new Error(`Invalid range: ${start}-${end}`);
+    }
+
+    let i = start;
+    while (i <= end) {
+      iterations += 1;
+      const numStr = i.toString();
+      if (numStr.length % 2) {
+        // odd length can't be doubled pattern
+        // jump to next power of 10 (even number of digits)
+        const left = 10 ** Math.floor(numStr.length / 2);
+        i = parseInt(`${left}${left}`, 10);
+        continue;
+      }
+      const left = numStr.slice(0, numStr.length / 2);
+      const right = numStr.slice(numStr.length / 2);
+      const leftNum = parseInt(left, 10);
+      const rightNum = parseInt(right, 10);
+      if (rightNum < leftNum) {
+        // jump to next doubled pattern
+        i = parseInt(`${left}${left}`, 10);
+        continue;
+      }
+
+      if (left === right) {
+        invalidIds.push(i);
+      }
+
+      const nextLeft = leftNum + 1;
+      // Consider the next doubled pattern
+      i = parseInt(`${nextLeft}${nextLeft}`, 10);
+    }
   }
 
-  let i = start;
-  while (i <= end) {
-    iterations += 1;
-    const numStr = i.toString();
-    if (numStr.length % 2) {
-      // odd length can't be doubled pattern
-      // jump to next power of 10 (even number of digits)
-      const left = 10 ** Math.floor(numStr.length / 2);
-      i = parseInt(`${left}${left}`, 10);
-      continue;
-    }
-    const left = numStr.slice(0, numStr.length / 2);
-    const right = numStr.slice(numStr.length / 2);
-    const leftNum = parseInt(left, 10);
-    const rightNum = parseInt(right, 10);
-    if (rightNum < leftNum) {
-      // jump to next doubled pattern
-      i = parseInt(`${left}${left}`, 10);
-      continue;
+  console.log('\n---- Part 1 ----');
+  console.log(`iterations: ${iterations}`); // 1949832 -> 799
+  console.log(`Number of invalid IDs: ${invalidIds.length}`); // 768
+  const sum = invalidIds.reduce((acc, curr) => acc + curr, 0);
+  console.log(`Sum of invalid IDs (answer): ${sum}`); // 15873079081
+};
+
+const part2 = () => {
+  const invalidIds = new Set<number>();
+  let iterations = 0;
+  for (const [start, end] of ranges) {
+    if (!start || !end || start > end) {
+      throw new Error(`Invalid range: ${start}-${end}`);
     }
 
-    if (left === right) {
-      invalidIds.push(i);
+    let i = start;
+    while (i <= end) {
+      const numStr = i.toString();
+      const numDigits = numStr.length;
+      for (let pLen = 1; pLen <= numDigits / 2; pLen += 1) {
+        if (numDigits % pLen) continue; // pattern can't fit evenly
+        iterations += 1;
+        const pat = numStr.slice(0, pLen);
+        const candidate = parseInt(pat.repeat(numDigits / pLen), 10);
+        if (candidate === i) {
+          invalidIds.add(i);
+        }
+      }
+      i += 1;
     }
-
-    const nextLeft = leftNum + 1;
-    // Consider the next doubled pattern
-    i = parseInt(`${nextLeft}${nextLeft}`, 10);
   }
-}
 
-console.log(`iterations: ${iterations}`); // 1949832 -> 799
-console.log(`Number of invalid IDs: ${invalidIds.length}`); // 768
-const sum = invalidIds.reduce((acc, curr) => acc + curr, 0);
-console.log(`Sum of invalid IDs: ${sum}`); // 15873079081
+  console.log('\n---- Part 2 ----');
+  console.log(`iterations: ${iterations}`); // 4960087
+  console.log(`Number of invalid IDs: ${invalidIds.size}`); // 842
+  const sum = Array.from(invalidIds).reduce((acc, curr) => acc + curr, 0);
+  console.log(`Sum of invalid IDs (answer): ${sum}`); // 22617871034
+};
+
+part1();
+part2();
