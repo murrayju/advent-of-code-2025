d5c31f592b9165ba497fc4030f141b7dcad89a18
Justin Murray <justin@murrayju.com>
2025-12-06 23:33:03 -0500

    day 4 part 2

--- day-4/README.md
+++ day-4/README.md
@@ -1,3 +1,7 @@
 # Day 4 Notes
 
 A first pass over the 2d grid with a constant (8) operations per cell to calculate the neighbor counts, and then a second pass to count the accessible cells based on the neighbor counts. This is linear with the number of cells in the grid, so at least close to optimal.
+
+## Part 2
+
+I'm not actually sure how to score my solution to part 2. It is performing a linear scan of the cells repeatedly in a loop, but it is < O(n^2) because it is bounded by how many rolls are actually removable. I measured that the outer loop iterated 33 times, which is _far_ from n (138^2).
--- day-4/index.ts
+++ day-4/index.ts
@@ -16,40 +16,60 @@ const width = grid[0]!.length;
 
 console.log(`Grid size: ${width}x${height}`); // 138x138
 
-// Part 1
-(() => {
-  const neighbors = Array.from({ length: height }, () =>
-    Array.from({ length: width }, () => 0),
-  );
+console.log('\n---- Part 1 ----');
+const neighbors = Array.from({ length: height }, () =>
+  Array.from({ length: width }, () => 0),
+);
 
-  const increment = (x: number, y: number) => {
-    if (x < 0 || x >= width || y < 0 || y >= height) return;
-    neighbors[y]![x]! += 1;
-  };
+const increment = (x: number, y: number, amount = 1) => {
+  if (x < 0 || x >= width || y < 0 || y >= height) return;
+  neighbors[y]![x]! += amount;
+};
+
+const incrementNeighbors = (x: number, y: number, amount = 1) => {
+  for (let xO = -1; xO <= 1; xO++) {
+    for (let yO = -1; yO <= 1; yO++) {
+      if (!xO && !yO) continue;
+      increment(x + xO, y + yO, amount);
+    }
+  }
+};
+
+for (const [y, row] of grid.entries()) {
+  for (const [x, cell] of row.entries()) {
+    if (!cell) continue;
+    incrementNeighbors(x, y);
+  }
+}
+
+const accessible = neighbors.reduce(
+  (acc, row, y) =>
+    acc + row.filter((count, x) => grid[y]![x]! && count < 4).length,
+  0,
+);
+
+console.log(`Accessible paper rolls (answer): ${accessible}`); // 1553
+
+console.log('\n---- Part 2 ----');
+let steps = 0;
+let removed = 0;
+let totalRemoved = 0;
+
+do {
+  steps++;
+  removed = 0;
 
   for (const [y, row] of grid.entries()) {
     for (const [x, cell] of row.entries()) {
-      if (!cell) continue;
-      increment(x - 1, y - 1);
-      increment(x - 1, y);
-      increment(x - 1, y + 1);
-      increment(x, y - 1);
-      increment(x, y + 1);
-      increment(x + 1, y - 1);
-      increment(x + 1, y);
-      increment(x + 1, y + 1);
+      if (!cell || neighbors[y]![x]! >= 4) continue;
+      grid[y]![x]! = false;
+      incrementNeighbors(x, y, -1);
+      removed++;
     }
   }
 
-  const accessible = neighbors.reduce(
-    (acc, row, y) =>
-      acc + row.filter((count, x) => grid[y]![x]! && count < 4).length,
-    0,
-  );
+  totalRemoved += removed;
+} while (removed);
 
-  console.log('\n---- Part 1 ----');
-  console.log(`Accessible paper rolls (answer): ${accessible}`); // 1553
-})();
-
-// Part 2
-(() => {})();
+console.log(`${steps} steps`); // 33
+console.log(`Total removable rolls (answer): ${totalRemoved}`); // 8442
